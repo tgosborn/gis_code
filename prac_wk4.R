8492b6042a5b308c9880da4722971fdba84efae5
tgosborn <thomas.osborn.20@ucl.ac.uk>
2020-10-30 14:20:59 +0000

    New code

--- prac_wk4.R
+++ prac_wk4.R
@@ -2,3 +2,17 @@ y <- c(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
 x <- c(2, 3, 2, 4, 1, 4, 6, 10)
 summary(x)
 summary(y)
+install.packages("reprex") # this installs the reprex package. This helps produce a reproducible example, for example if you need to trouble shoot. 
+library(reprex) 
+reprex() # shows whats in the clipboard. 
+
+# Hello this is a test of git and github 
+
+x <- 1+1
+
+y <- 2+2
+
+z <- 5+5
+
+
+
